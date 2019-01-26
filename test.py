b82dcf0bcbe7c930f53413fdd9d4efe7e803194a
Runtian <runtianzhai@gmail.com>
2019-01-26 19:49:12 +0800

    aaa

---
+++ test.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+import cifar10_input
+
+
+if __name__ == '__main__':
+  import json
+  import sys
+  import math
+
+
+  from model import Model
+
+  with open('config.json') as config_file:
+    config = json.load(config_file)
+
+  model_file = tf.train.latest_checkpoint(config['model_dir'])
+  if model_file is None:
+    print('No model found')
+    sys.exit()
+
+  model = Model(mode='eval')
+
+  with tf.Session() as sess:
+    # Restore the checkpoint
+    saver.restore(sess, model_file)
+
+    # Iterate over the samples batch-by-batch
+    num_eval_examples = 10000
+    eval_batch_size = 100
+    num_batches = int(math.ceil(num_eval_examples / eval_batch_size))
+    total_corr = 0
+
+    for ibatch in range(num_batches):
+      bstart = ibatch * eval_batch_size
+      bend = min(bstart + eval_batch_size, num_eval_examples)
+      print('batch size: {}'.format(bend - bstart))
+
+      x_batch = cifar.eval_data.xs[bstart:bend, :]
+      y_batch = cifar.eval_data.ys[bstart:bend]
+
+      dict_normal = {model.x_input: x_batch,
+                  model.y_input: y_batch}
+      cur_corr, y_pred_batch = sess.run([model.num_correct, model.predictions],
+                                        feed_dict=dict_normal)
+      total_corr += cur_corr
+
+    accuracy = total_corr / num_eval_examples
+    print('Accuracy: {:.2f}%'.format(100.0 * accuracy))
